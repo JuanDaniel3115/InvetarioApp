a962f3d9205933281c514685b9e6de29f848f5c1
Boris Monroy Gachancipa <bmonroyg@emeal.nttdata.com>
2026-06-24 21:32:36 -0500

    Agregar clase Filemanager para manejo de archivos y actualizar inventario

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿using InventarioApp.Factories;
 using InventarioApp.Repositories;
 using InventarioApp.Models;
+using InventarioApp.Infrastructure;
 
 Console.WriteLine("===== InventarioApp =======");
+
+var fileManager = new Filemanager();
+string contenido = "Inventario Actualizado";
+fileManager.Escribir("inventario.txt",contenido);
+
+string leerContenido = fileManager.Leer("inventario.txt");
+Console.WriteLine(contenido);
 
 var repository = new InMemoryProductoRepository();
 var laptop = ProductoFactory.Crear(nombre: "Laptop Dell XPS 13", precio: 1200, cantidad: 5, CategoriaProducto.Electronica);
@@ -17,27 +25,27 @@ repository.Agregar(teclado);
 repository.Agregar(silla);
 repository.Agregar(escritorio);
 
-Console.WriteLine($"Productos Agregados: {repository.Cantidad}\n");
+// Console.WriteLine($"Productos Agregados: {repository.Cantidad}\n");
 
 //Consultas Basicas LINQ
 var electronicos = repository.BuscarPorCategoria(CategoriaProducto.Electronica);
 
-Console.WriteLine($"Prodcutos de electronica: ");
+// Console.WriteLine($"Prodcutos de electronica: ");
 
 foreach(var producto in electronicos)
 {
-    Console.WriteLine($"{producto.Nombre} : ${producto.Precio}");
+    // Console.WriteLine($"{producto.Nombre} : ${producto.Precio}");
 }
 var conMouse = repository.BuscarPorNombre("mouse");
-Console.WriteLine($"\nProductos con Mouse en el nombre:");
+// Console.WriteLine($"\nProductos con Mouse en el nombre:");
 foreach(var producto in conMouse)
 {
-    Console.WriteLine($"{producto.Nombre} ");
+    // Console.WriteLine($"{producto.Nombre} ");
 
 }
 
 var nombres = repository.ObtenerNombres();
-Console.WriteLine($"\nTodos los nombre {string.Join(",",nombres)}");
+// Console.WriteLine($"\nTodos los nombre {string.Join(",",nombres)}");
 
 var hayStockBajo = repository.HayStockBajo();
-Console.WriteLine($"\n Hay stock bajo ? {hayStockBajo}");
+// Console.WriteLine($"\n Hay stock bajo ? {hayStockBajo}");
